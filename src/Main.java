ea73b6fa4bcbe9e2b80247677749dfdfb3d5280d
Saddam-Alhakami <117770757+Saddam-Alhakami@users.noreply.github.com>
2024-09-13 18:40:54 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 import java.util.Stack;
 import java.util.Scanner;
 public class Main {
